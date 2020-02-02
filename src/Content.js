f88e3a3b921b8ebd6951dc1d5df84cd33b992162
Natalya Myunster <natalyamyunster@yahoo.com>
2020-02-02 01:29:29 +0100

    Content, Footer, Menu added

--- src/Content.js
+++ src/Content.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function Content() {
   return (
-    <div className="App">
+    <div className="App-content">
       Content
     </div>
   );
--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Logo from "./Logo";
 
 function Footer() {
   return (
-    <div className="App">
-      Footer
+    <div className="App-footer">
+        <div className="App-logo">
+            <Logo/>
+        </div>
+        <p className="Footer-paragraph">
+            Footer
+        </p>
     </div>
   );
 }
