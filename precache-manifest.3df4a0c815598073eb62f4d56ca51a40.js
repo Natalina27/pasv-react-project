085625735f3ffa4d382aaf6dcfc8206899d23401
Natalya Myunster <natalyamyunster@yahoo.com>
2020-02-03 17:57:41 +0100

    Updates

---
+++ precache-manifest.3df4a0c815598073eb62f4d56ca51a40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd4c2c06daf4c44b59a06f932f850d5",
+    "url": "/react-pasv-project/index.html"
+  },
+  {
+    "revision": "2e9430d83426c91a0628",
+    "url": "/react-pasv-project/static/css/main.980a0baa.chunk.css"
+  },
+  {
+    "revision": "4c9d23639dea79ca2991",
+    "url": "/react-pasv-project/static/js/2.cc6cf586.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-pasv-project/static/js/2.cc6cf586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e9430d83426c91a0628",
+    "url": "/react-pasv-project/static/js/main.3a5dd1a4.chunk.js"
+  },
+  {
+    "revision": "ff6ada753776daa43fe3",
+    "url": "/react-pasv-project/static/js/runtime-main.1a47f85f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pasv-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
