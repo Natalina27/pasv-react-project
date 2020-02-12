665dad8c017dc0b0d7f590d6d214dc20b59615cc
Natalya Myunster <natalyamyunster@yahoo.com>
2020-02-12 21:08:13 +0100

    added function clickButton in content

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import './App.css';
 
 function Content() {
+
+    function clickButton(){
+        console.log('Clicked !' );
+    }
   return (
     <main className="App-content">
       <h3> First App</h3>
+        <button>Add one</button>
+        <button>Add two</button>
         <p>
             Content
         </p>
